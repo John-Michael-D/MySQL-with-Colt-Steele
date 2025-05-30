62438634a64fd558b890f7d7737963aeeed5792a
John-Michael-D <johnm.dantes@yahoo.com>
2025-05-29 20:44:56 -0400

    Create Lesson 20 - UPDATE Exercise.sql

---
+++ Lesson 20 - UPDATE Exercise.sql
@@ -0,0 +1,4 @@
+UPDATE cats SET name='Jack' WHERE breed ='Sphynx';
+UPDATE cats SET breed='British Shorthar' WHERE name='Ringo';
+UPDATE cats SET age=12 WHERE breed='Maine Coon';
+SELECT * FROM cats;
